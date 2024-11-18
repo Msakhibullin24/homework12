a01a09a6da069d4570613f3f059dd0a1e33ebdf0
Минтик <121098677+Msakhibullin24@users.noreply.github.com>
2024-11-18 15:34:06 +0300

    задание 2 в 1 части

---
+++ zadanie2.py
@@ -0,0 +1,13 @@
+import numpy as np
+mean = 0  # математическое ожидание
+variance = 5  # дисперсия
+std_dev = np.sqrt(variance)  # стандартное отклонение
+
+random_vector = np.random.normal(mean, std_dev, 2000) # Формирование вектора из 2000 случайных величин
+
+# Вычисление экспериментального математического ожидания и дисперсии
+experimental_mean = np.mean(random_vector)
+experimental_variance = np.var(random_vector)
+
+print("Экспериментальное математическое ожидание (среднеезначение значение):", experimental_mean)
+print("Экспериментальная дисперсия:", experimental_variance)

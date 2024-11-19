77cccc24fd6036de42ffb84a2c6e0d18a1d61e02
Минтик <121098677+Msakhibullin24@users.noreply.github.com>
2024-11-19 09:38:40 +0300

    Add files via upload

--- chast1zadanie5.py
+++ chast1zadanie5.py
@@ -1,21 +1,31 @@
 import pandas as pd
 
-# Чтение данных из файла
-saved_data = pd.read_csv("first_1000_rows.csv")
-
-# Проверка названий столбцов
-print(saved_data.columns)
+saved_data = pd.read_csv("first_1000_rows.csv") # по стандарту вставляем что имеем
 
 # Удаление пробелов из названий
 saved_data.columns = saved_data.columns.str.strip()
-
 # Подсчет уникальных значений
-unique_count = saved_data["Rel Hum (%)"].nunique()  # или правильное имя
+unique_count = saved_data["relative humidity (%)"].nunique()
 total_count = len(saved_data)
 percentage_unique = (unique_count / total_count) * 100
 
-print(f"Уникальных значений: {unique_count}")
-print(f"Процент уникальных значений: {percentage_unique:.2f}%")
+print(f"Уникальных значений {unique_count}")
+print(f"Процент уникальных значений {percentage_unique:.2f}%")
+'''
+import pandas as pd
 
+saved_data = pd.read_csv("first_1000_rows.csv")     # база сохранненая мы из нее вытаскиваем данные 
+saved_data["date-time"] = pd.to_datetime(saved_data["date-time"])   # Преобразуем столбец date-time в формат datetime
+filtered_data = saved_data[saved_data["date-time"].dt.year > 2017]    # Фильтрация данных старше 2017 года у нас тут по 0 будет тк у нас данные 2015 годаа)
+unique_count_filtered = filtered_data["relative humidity (%)"].nunique()    # Количество уникальных значений в столбце `relative humidity (%)` для отфильтрованных данных
+total_count_filtered = len(filtered_data)   # Общее количество строк в отфильтрованных данных
+# иф элс на выялвеение поцента уникальных значений
+if total_count_filtered > 0:
+    percentage_unique_filtered = (unique_count_filtered / total_count_filtered) * 100
+else:
+    percentage_unique_filtered = 0
 
-#ff
+print(f"Уникальных значений (для данных после 2017 года): {unique_count_filtered}")
+print(f"Процент уникальных значений: {percentage_unique_filtered:.2f}%")
+
+'''
--- chast1zadanie6.py
+++ chast1zadanie6.py
@@ -1,18 +1,42 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 
 saved_data = pd.read_csv("first_1000_rows.csv")
+saved_data.columns = saved_data.columns.str.strip() # Удаляем лишние пробелы из названий столбцов
 
-# Топ-10 значений
-top_10_windspeed = saved_data.nlargest(10, "windspeed (m/s)")
+saved_data['date-time'] = pd.to_datetime(saved_data['date-time'], errors='coerce')   # Преобразуем столбец даты в формат datetime
 
-# Построение графика
+top_10_windspeed = saved_data.nlargest(10, "wind speed (m/s)") # Находим 10 самых больших значений скорости ветра
+top_10_windspeed.plot(
+    kind="bar",
+    x="date-time",  #
+    y="wind speed (m/s)",
+    color="blue",
+    figsize=(10, 6),
+    title="Top-10 значений скорости ветра (м/с)"
+)
+
+plt.show()
+
+'''
+import pandas as pd
+import matplotlib.pyplot as plt
+
+saved_data = pd.read_csv("first_1000_rows.csv")
+saved_data.columns = saved_data.columns.str.strip()   # Удаляем лишние пробелы из названий столбцов
+saved_data['date-time'] = pd.to_datetime(saved_data['date-time'], errors='coerce')  # Преобразуем столбец даты в формат datetime
+
+filtered_data = saved_data[saved_data['date-time'].dt.year > 2017]
+top_10_windspeed = filtered_data.nlargest(10, "wind speed (m/s)") # Находим 10 самых больших значений скорости ветра
+
+
+print(top_10_windspeed)
+
 plt.figure(figsize=(10, 6))
-plt.bar(top_10_windspeed.index, top_10_windspeed["windspeed (m/s)"], color='blue')
-plt.title("Топ-10 значений скорости ветра", fontsize=16)
+plt.bar(top_10_windspeed.index, top_10_windspeed["wind speed (m/s)"], color='blue')
+plt.title("Top-10 значений скорости ветра (м/с), старше 2017 года", fontsize=16)
 plt.xlabel("Индексы строк", fontsize=12)
 plt.ylabel("Скорость ветра (м/с)", fontsize=12)
 plt.grid(alpha=0.3)
 plt.show()
-
-#ff
+'''
